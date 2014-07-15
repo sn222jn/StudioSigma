11c1a635b000ec6291ff5c8d1a029d3cb08cea39
Sylvester Norrbjörk <sn222jn@student.lnu.se>
2014-07-15 20:30:21 +0200

    site with latest flash

---
+++ js/script.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function () {
+
+	$(".content ol ul").hide();
+
+	$(".content ol li").click(function (e) {
+		$(this).children("ul").toggle(
+			{ left: 200 }, {
+				duration: 'slow',
+				easing: 'linear'
+			})
+		return false;
+		e.stopPropagation();
+	});
+
+
+
+});
+
+$('.accordion li').on('mouseenter', function () {
+	$(this).find('.menuItem').show(
+	{
+		duration: 'normal',
+		position: 'absolute',
+		slide: 'down'
+	});
+}).on('mouseleave', function () {
+	$(this).find('.menuItem').hide(
+	{
+		duration: 'normal'
+	});
+});
